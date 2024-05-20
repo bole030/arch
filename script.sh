aab6e47c56bf3fe1aac2f3079b164fb353466057
bole030 <92758021+bole030@users.noreply.github.com>
2024-05-20 16:58:40 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+
+echo "Please enter EFI paritition: (example /dev/sda1 or /dev/nvme0n1p1)"
+read EFI
+
+echo "Please enter SWAP paritition: (example /dev/sda2)"
+read SWAP
+
+echo "Please enter Root(/) paritition: (example /dev/sda3)"
+read ROOT 
+
+echo -e "\n create and mount partitions \n"
+
+mkfs.vfat -F32 -n "EFISYSTEM" "${EFI}"
+mkswap "${SWAP}"
+swapon "${SWAP}"
+mkfs.ext4 "${ROOT}"
+
+# mount target
+mount -t ext4 "${ROOT}" /mnt
+mkdir /mnt/boot
+mount "${EFI}" /mnt/boot/
+
+echo "installing arch base"
+pacstrap /mnt base base-devel --noconfirm --needed
+
+echo "installing linux"
+pacstrap /mnt linux linux-firmware --noconfirm --needed
+
+echo "installing basic tools"
+
+pacstrap /mnt zsh networkmanager vim sof-firmware grub efibootmgr --noconfirm --needed
+
+# fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+echo "-- installing bootloader  --"
+bootctl install --path /mnt/boot
+echo "default arch.conf" >> /mnt/boot/loader/loader.conf
+cat <<EOF > /mnt/boot/loader/entries/arch.conf
+title Arch Linux
+linux /vmlinuz-linux
+initrd /initramfs-linux.img
+options root=${ROOT} rw
+EOF
+
+echo "Please enter your hostname"
+read HOSTNAME 
+
+echo "Please enter your password"
+read PASSWORD 
+
+cat <<REALEND > /mnt/next.sh
+useradd -m bole
+usermod -aG wheel,storage,power,audio $USER
+echo bole:$PASSWORD | chpasswd
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+
+echo "set language and set locale"
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+echo "set time"
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+hwclock --systohc
+
+echo "${HOSTNAME}" > /etc/hostname
+cat <<EOF > /etc/hosts
+127.0.0.1	localhost
+::1			localhost
+127.0.1.1	${HOSTNAME}.localdomain	${HOSTNAME}
+EOF
+
+echo "installing compressing stuff"
+pacman -S zip unzip --noconfirm --needed
+
+echo "installing remote stuff"
+pacman -S bluez bluez-utils openssl openssh ufw rsync git --noconfirm --needed
+
+echo "installing bluetooth stuff"
+pacman -S bluez bluez-utils --noconfirm --needed
+
+echo "installing display stuff"
+pacman -S xorg --noconfirm --needed
+
+echo "installing audio stuff"
+pacman -S pulseaudio --noconfirm --needed
+
+systemctl enable NetworkManager bluetooth
+
+echo "finished. type reboot."
+
+REALEND
+
+
+arch-chroot /mnt sh next.sh
