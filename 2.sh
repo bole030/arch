686a2d935fcc0b9e97783821c76c3b657e99863c
bole030 <92758021+bole030@users.noreply.github.com>
2024-05-21 01:57:39 +0200

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -2,4 +2,17 @@ sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow 22
 
+echo "installing browsers"
+sudo pacman -S firefox discord chromium xdotool xclip tigervnc man-db
 
+echo "installing browsers"
+sudo pacman -S discord
+
+echo "installing xdotool and xclip"
+sudo pacman -S xdotool xclip
+
+echo "installing manuals"
+sudo pacman -S man-db
+
+echo "installing vnc"
+sudo pacman -S tigervnc
