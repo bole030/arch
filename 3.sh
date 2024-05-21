88a2391557de968fcec8d6645527aab5d5b0cd7b
bole030 <92758021+bole030@users.noreply.github.com>
2024-05-21 18:42:59 +0200

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,2 +1,4 @@
 sudo pacman -S nvidia picom fuse3 os-prober
-sed -i 's/^#%wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+sudo sed -i 's/^GRUB_TIMEOUT=5/GRUB_TIMEOUT=20/' /etc/default/grub
+sudo sed -i 's/^#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
+sudo sed -i 's/^# GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
