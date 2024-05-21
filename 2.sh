273b28ded608ef843f0e3d877a6b17f00e326520
bole030 <92758021+bole030@users.noreply.github.com>
2024-05-21 02:10:03 +0200

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,6 +1,27 @@
+
+echo "installing compressing stuff"
+pacman -S zip unzip --noconfirm --needed
+
+echo "installing bluetooth stuff"
+pacman -S bluez bluez-utils --noconfirm --needed
+sudo systemctl enable bluetooth.service
+sudo systemctl start bluetooth.service
+
+echo "installing remote stuff (ssh, ufw, rsync, git)"
+pacman -S openssl openssh ufw rsync git --noconfirm --needed
+sudo systemctl enable sshd
+sudo systemctl start sshd
+
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow 22
+sudo ufw enable
+
+echo "installing display stuff"
+pacman -S xorg --noconfirm --needed
+
+echo "installing audio stuff"
+pacman -S pulseaudio --noconfirm --needed
 
 echo "installing browsers"
 sudo pacman -S firefox discord chromium xdotool xclip tigervnc man-db
