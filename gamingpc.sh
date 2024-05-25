8a0c79c1684f4c5bd8dbe6b193b4b9622f8d9047
bole030 <92758021+bole030@users.noreply.github.com>
2024-05-25 21:01:16 +0200

    Delete gamingpc.sh

--- gamingpc.sh
+++
@@ -1,4 +0,0 @@
-sudo pacman -S nvidia picom fuse3 os-prober
-sudo sed -i 's/^GRUB_TIMEOUT=5/GRUB_TIMEOUT=20/' /etc/default/grub
-sudo sed -i 's/^#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
-sudo sed -i 's/^# GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
