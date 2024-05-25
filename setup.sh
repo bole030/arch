6cf75d24e8475108509de0f3267d8e16cc6c13a1
bole030 <92758021+bole030@users.noreply.github.com>
2024-05-25 21:14:17 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -139,9 +139,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 echo "installing python"
 sudo pacman -S python
 
-dbus-update-activation-environment --all
-gnome-keyring-daemon --start --components=secrets
-
 sudo pacman -S nvidia picom fuse3 os-prober
 sudo sed -i 's/^GRUB_TIMEOUT=5/GRUB_TIMEOUT=20/' /etc/default/grub
 sudo sed -i 's/^#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
